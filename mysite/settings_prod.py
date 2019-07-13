22e0685559b6205c0c56a1124df40b6fe0733c0a
chyngyzs1 <0777755169ch@gmail.com>
2019-07-13 16:42:24 +0600

    coding is easy

---
+++ mysite/settings_prod.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+DATABASES = {
+
+   'default': {
+
+Copy
+   'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   'NAME': 'simpledjango',
+   'USER': 'simpledjangouser',
+   'PASSWORD': '123simpledjango123',
+   'HOST': 'localhost',
+   'PORT': '',
+   }
+
+}
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chyngyz/Documents/Django/todolist/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
